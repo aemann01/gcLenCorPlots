fd50a73f48bb424f1c23ab4244853983bd80f109
aemann01 <aemann01@ou.edu>
2019-10-23 16:28:09 -0500

    cleaning up

--- gcLenCorPlots.py
+++ gcLenCorPlots.py
@@ -74,7 +74,7 @@ def plotDat(dfGrouped):
 	plt.savefig('%s_plot.pdf' % args.input)
 
 def main():
-	print("Author: Allison E. Mann (allison.mann@botany.ubc.ca)")
+	print("Author: Allison E. Mann")
 	print("Cite: Mann AE et al. 2018. Differential preservation of endogenous human and microbial DNA in dental calculus and dentin. Scientific Reports 8:9822")
 	print("Copyright 2018: GPLv3.0\n")
 	assert os.path.exists(args.input), 'Error! File does not exist: %s. Is the path correct?' % args.input
